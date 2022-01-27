56fa2bd5f0ed7a0ab64fa5b1bcfc8349c2ce2277
MariusDevelops <brazasmarius@gmail.com>
2022-01-27 22:29:21 +0100

    JS calculations

---
+++ script.js
@@ -0,0 +1,33 @@
+let display = document.querySelector(".display");
+
+let buttons = Array.from(document.getElementsByClassName("button"));
+// console.log(buttons);
+
+buttons.map((button) => {
+  button.addEventListener("click", (e) => {
+    // console.log("clicked");
+    // console.group(e);
+    // console.log(e.target);
+    // console.log(e.target.innerText);
+    switch (e.target.innerText) {
+      case "RESET":
+        display.innerText = "";
+        break;
+      case "DEL":
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
+        }
+        break;
+      case "=":
+        try {
+          display.innerText = eval(display.innerText);
+        } catch {
+          display.innerText = "Error!";
+        }
+
+        break;
+      default:
+        display.innerText += e.target.innerText;
+    }
+  });
+});
